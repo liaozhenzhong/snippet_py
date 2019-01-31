4d244d83b7b45efef884700dcc0b494e44aceba7
liaozhenzhong <liaozhenzhong@gmail.com>
2019-01-31 12:03:56 -0500

    more

--- test_smallest_rotation_highest_score.py
+++ test_smallest_rotation_highest_score.py
@@ -1,7 +1,38 @@
 class Solution:
     def bestRotation(self, A):
+        N = len(A)
+        B = [0] * N
+        for i, x in enumerate(A):
+            if x > i:
+                j = i + 1
+                B[j] += 1
+
+                j = i + 1 + N - x
+                if j < N:
+                    B[j] -= 1
+
+            else:
+                j = 0
+                B[j] += 1
+
+                j = i - x + 1
+                if j < N:
+                    B[j] -= 1
+
+                j = i + 1
+                if j < N:
+                    B[j] += 1
+
+        for i in range(1, N):
+            B[i] += B[i - 1]
+        C = max(B)
+        for i in range(N):
+            if B[i] == C:
+                return i
+
+    def bestRotation2(self, A):
         L = len(A)
-        A = A*2
+        A = A * 2
         best_points = 0
         best_k = 0
         for k in range(L):
@@ -17,18 +48,24 @@ class Solution:
 
 def test_best_rotation():
     X = [2, 3, 1, 4, 0]
-    assert(Solution().bestRotation(X) == 3)
+    assert (Solution().bestRotation(X) == 3)
 
 
 def test_best_rotation_2():
     X = [0, 1, 2]
-    assert(Solution().bestRotation(X) == 0)
+    assert (Solution().bestRotation(X) == 0)
+
 
 def test_best_rotation_3():
     X = [3, 0, 1, 2]
-    assert(Solution().bestRotation(X) == 1)
+    assert (Solution().bestRotation(X) == 1)
 
 
 def test_best_rotation_4():
     X = [1, 3, 0, 2, 4]
-    assert(Solution().bestRotation(X) == 0)
+    assert (Solution().bestRotation(X) == 0)
+
+
+def test_best_rotation_5():
+    X = [6, 2, 8, 3, 5, 2, 4, 3, 7, 6]
+    assert (Solution().bestRotation(X) == 9)
