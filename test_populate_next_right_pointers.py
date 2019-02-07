568b5acc00800c09496a2f621b161188be87b02c
liaozhenzhong <liaozhenzhong@gmail.com>
2019-02-06 23:15:26 -0500

    more

---
+++ test_populate_next_right_pointers.py
@@ -0,0 +1,74 @@
+# Definition for binary tree with next pointer.
+# class TreeLinkNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+#         self.next = None
+
+class TreeLinkNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+        self.next = None
+
+
+from collections import deque
+
+
+class Solution:
+    # @param root, a tree link node
+    # @return nothing
+    def connect(self, root):
+        to_visit = deque()
+        to_visit.append([root])
+        while to_visit:
+            nodes = to_visit.popleft()
+            for i in range(len(nodes) - 1):
+                nodes[i].next = nodes[i + 1]
+            new_row = []
+            for n in nodes:
+                if n.left:
+                    new_row.append(n.left)
+                if n.right:
+                    new_row.append(n.right)
+            if new_row:
+                to_visit.append(new_row)
+
+    def __init__(self):
+        self.root = None
+
+    def add(self, new_node, node):
+        if not self.root:
+            self.root = new_node
+            return
+
+        if new_node.val < node.val:
+            if node.left:
+                self.add(new_node, node.left)
+            else:
+                node.left = new_node
+        else:
+            if node.right:
+                self.add(new_node, node.right)
+            else:
+                node.right = new_node
+
+    def order(self, node):
+        if not node:
+            return
+
+        yield from self.order(node.left)
+        yield node.val
+        yield from self.order(node.right)
+
+
+so = Solution()
+so.add(TreeLinkNode(2), so.root)
+so.add(TreeLinkNode(1), so.root)
+so.add(TreeLinkNode(3), so.root)
+for i in so.order(so.root):
+    print(i)
+so.connect(so.root)
+so.connect(so.root)
---
+++ test_unique_path_II.py
@@ -0,0 +1,79 @@
+class Solution:
+    def uniquePathsWithObstacles(self, obstacleGrid: 'List[List[int]]') -> 'int':
+        if not obstacleGrid or not obstacleGrid[0]:
+            return 0
+
+        H = len(obstacleGrid)
+        W = len(obstacleGrid[0])
+        ways = [[0] * W for _ in range(H)]
+
+        for i in range(W):
+            if obstacleGrid[0][i] == 1:
+                break
+            ways[0][i] = 1
+
+        for i in range(H):
+            if obstacleGrid[i][0] == 1:
+                break
+            ways[i][0] = 1
+
+        for i in range(1, H):
+            for j in range(1, W):
+                if obstacleGrid[i][j] == 1:
+                    continue
+                ways[i][j] = ways[i - 1][j] + ways[i][j - 1]
+
+        return ways[-1][-1]
+
+
+def test_uniquePathsWithObstacles():
+    x = [
+        [0, 0, 0],
+        [0, 1, 0],
+        [0, 0, 0]
+    ]
+
+    res = Solution().uniquePathsWithObstacles(x)
+    assert res == 2
+
+
+def test_uniquePathsWithObstacles_2():
+    x = [
+        [0, 0, 0, 0, 0],
+        [0, 1, 0, 1, 0],
+        [0, 0, 0, 0, 0]
+    ]
+
+    res = Solution().uniquePathsWithObstacles(x)
+    assert res == 3
+
+
+def test_uniquePathsWithObstacles_3():
+    x = [
+        [0, 0],
+        [0, 0],
+    ]
+
+    res = Solution().uniquePathsWithObstacles(x)
+    assert res == 2
+
+
+def test_uniquePathsWithObstacles_4():
+    x = [
+        [0, 0, 0],
+        [0, 0, 0],
+        [0, 0, 0],
+    ]
+
+    res = Solution().uniquePathsWithObstacles(x)
+    assert res == 6
+
+
+def test_uniquePathsWithObstacles_5():
+    x = [[]]
+
+    res = Solution().uniquePathsWithObstacles(x)
+    assert res == 0
+
+
+test_uniquePathsWithObstacles_5()
